c4807254557a8ff730836d6fd682828396cae6c7
David Welch <david@davidwelch.co>
2012-07-02 19:46:28 -0600

    Included deps for File upload support. Removed Context-bootstrapper so only one container. Added basic webapp support for tags, JSTL, etc

--- src/main/resources/archetype-resources/src/main/java/config/MvcConfiguration.java
+++ src/main/resources/archetype-resources/src/main/java/config/MvcConfiguration.java
@@ -6,6 +6,8 @@ package ${package}.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -30,5 +32,8 @@ public class MvcConfiguration extends WebMvcConfigurerAdapter{
 		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
 	}
 
-	
+	@Bean
+	public MultipartResolver multipartResolver(){
+		return new CommonsMultipartResolver();
+	}
 }
